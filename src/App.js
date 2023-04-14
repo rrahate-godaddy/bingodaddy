b1c982d0071e0d603e62af2288bb349c7fbdeec6
Rahul Rahate <rrahate@godaddy.com>
2023-04-14 11:09:01 -0500

    Deploy gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
+import { useState, useEffect } from 'react';
+import { FireApp, Auth }  from "./firebaseConfig/firebaseConfig.js"
 import logo from './logo.svg';
 import './App.css';
+import {
+  createBrowserRouter,
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+import RouterError from './pages/router/routerError.js';
+import Home from './pages/home.js';
+import GameSession from './pages/gameSession.js';
+import GameEnd from './pages/gameEnd.js';
+import Form from './components/atoms/form.js';
+
 
 function App() {
+
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <Home />,
+      errorElement: <RouterError />,
+    },
+    {
+      path: "/:gameid",
+      element: <GameSession />,
+      errorElement: <RouterError />,
+    },
+    {
+      path: "/:gameid/end",
+      element: <GameEnd />,
+      errorElement: <RouterError />,
+    }
+  ]);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
+  
 }
 
 export default App;
